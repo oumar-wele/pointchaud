2c8d4157d54c4856ec3ba829173134599b5ebf0b
Wele <omwelepro@gmail.com>
2021-05-05 18:53:55 +0200

    add create annonce page and symfony boostrap form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,8 @@ use App\Entity\Annonce;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class HomeController extends AbstractController
 {
@@ -16,7 +18,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $repository= $this->getDoctrine()->getRepository(Annonce::class);
-         $annonces=$repository->findAll();
+         $annonces=$repository->findBy([],['createdAt'=>'DESC']);
      
         $title = "Welcome Hone";
         return $this->render('page/home.html.twig', [
@@ -36,6 +38,51 @@ class HomeController extends AbstractController
             'title' => $title,
             'annonce'=>$annonce,
         ]);
+    }
+
+    /**
+     * @Route("/creer-une-annonce", name="create_annoce")
+     */
+    public function creerAnnonce(Request $requet): Response
+    {
+        $form = $this->createFormBuilder(null,['action'=>'creer-une-annonce','method'=>'POST'])
+                ->add('title')
+                ->add('description',TextareaType::class)
+                ->getForm();
+                $form->handleRequest($requet);
+        if($form->isSubmitted() && $form->isValid() ){
+            $data= $form->getData();
+            $annonce= new Annonce();
+
+            $annonce->setTitle($data["title"])
+                      ->setDescription($data["description"]);
+            $em= $this->getDoctrine()->getManager();
+            $em->persist($annonce);
+            $em->flush();
+            
+
+            $this->addFlash('success', 'Inscription  realisé avec succes!');
+            return $this->redirectToRoute("app_home");
+
+        }
+        $title = "Creer votre annonce";
+         return  $this->render('page/creerAnnonce.html.twig',[
+             'title'=>$title,
+             'formulaire'=>$form->createView(),
+
+         ]);
+
+    }
+        /**
+     * @Route("/editer-une-annonce", name="edit_annoce")
+     */
+    public function etdit(): Response
+    {
+        $title = "Editer votre annonce";
+        return $this->render('page/edit.html.twig', [
+            'title' => $title,
+        ]);
+
     }
 
 }
